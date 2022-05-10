7716be49025c428ca000552c9741484507b4a6eb
Viktor <vktrgavryliuk@gmail.com>
2022-05-10 14:31:06 +0300

    password input

--- form.js
+++ form.js
@@ -40,7 +40,6 @@ function generatePasswords() {
   const pushCharactersByType = (type, arr) => {
     const typeAmount = getRandomInRange(length / arr.length, 2);
 
-
     for (let index = 0; index < typeAmount; index++) {
       password.push(type[getRandomInt(type.length)])
     }
@@ -49,9 +48,9 @@ function generatePasswords() {
   const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  const symbols = ["!", "#", "$", "&", "%", "'", "(", ")", "*", "+", "-", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "~", "`", "{", "|", "}", "."];
 
-  const types = [uppercase, lowercase, numbers, symbols]
+
+  const types = [uppercase, lowercase, numbers]
   types.forEach((type) => pushCharactersByType(type, types))
 
   const result = shuffle(password).join('');
